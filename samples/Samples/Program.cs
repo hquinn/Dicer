2cd22fe01f344d55018b8d26c62bf10732ed367a
Hayden Quinn <hayden@increnaut.io>
2021-10-11 20:38:40 +1000

    ✨ Created Response Formatter for outputting results

--- samples/Samples/Program.cs
+++ samples/Samples/Program.cs
@@ -4,71 +4,56 @@ using Dicer.Rollers;
 using Dicer.Rounding;
 using System;
 using System.Linq;
+using Dicer.Formatter;
 using static Dicer.Parser.Parser;
 
-BasicArithmetic();
-DiceExample();
-KeepLowestExample();
-BasicParsingExample();
-ComplexParsingExample();
-BasicRepeatingNodeExample();
+var formatter = new DefaultResponseFormatter();
 
-static void BasicArithmetic()
+BasicArithmetic(formatter);
+DiceExample(formatter);
+KeepLowestExample(formatter);
+BasicParsingExample(formatter);
+ComplexParsingExample(formatter);
+BasicRepeatingNodeExample(formatter);
+
+static void BasicArithmetic(IResponseFormatter formatter)
 {
 	var node = new AddNode(new NumberNode(1), new MultiplyNode(new NumberNode(2), new NumberNode(3)));
 	var response = node.Evaluate(new MinRoller(), new NoRounding());
-	Console.WriteLine($"BasicArithmetic Result: {response.Result}");
+	Console.WriteLine(formatter.Format(response, "BasicArithmetic"));
 }
 
-static void DiceExample()
+static void DiceExample(IResponseFormatter formatter)
 {
 	var node = new AddNode(new DiceNode(new NumberNode(4), new NumberNode(6), new NumberNode(3)), new DiceNode(new NumberNode(2), new NumberNode(10)));
 	var response = node.Evaluate(new RandomRoller(), new RoundToCeiling());
-	Console.WriteLine($"DiceExample Result: {response.Result}");
-	OutputRolls(response);
+	Console.WriteLine(formatter.Format(response, "DiceExample"));
 }
 
-static void KeepLowestExample()
+static void KeepLowestExample(IResponseFormatter formatter)
 {
 	var node = new DiceNode(new NumberNode(4), new NumberNode(6), new NumberNode(-3));
 	var response = node.Evaluate(new RandomRoller(), new RoundToNearest());
-	Console.WriteLine($"KeepLowestExample Result: {response.Result}");
-	OutputRolls(response);
+	Console.WriteLine(formatter.Format(response, "KeepLowestExample"));
 }
 
-static void BasicParsingExample()
+static void BasicParsingExample(IResponseFormatter formatter)
 {
 	var node = Parse("4D6K3 + 2d10");
 	var response = node.Evaluate(new RandomRoller(), new RoundToCeiling());
-	Console.WriteLine($"BasicParsingExample Result: {response.Result}");
-	OutputRolls(response);
+	Console.WriteLine(formatter.Format(response, "BasicParsingExample"));
 }
 
-static void ComplexParsingExample()
+static void ComplexParsingExample(IResponseFormatter formatter)
 {
 	var node = Parse("4D(5/(1+2))K-3 +- (5*(1+2))d10");
 	var response = node.Evaluate(new RandomRoller(), new RoundToCeiling());
-	Console.WriteLine($"ComplexParsingExample Result: {response.Result}");
-	OutputRolls(response);
+	Console.WriteLine(formatter.Format(response, "ComplexParsingExample"));
 }
 
-static void BasicRepeatingNodeExample()
+static void BasicRepeatingNodeExample(IResponseFormatter formatter)
 {
 	var node = Parse("4D6K3", "6");
 	var responses = node.Evaluate(new RandomRoller(), new RoundToCeiling());
-
-	int counter = 1;
-	foreach (var response in responses)
-	{
-		Console.WriteLine($"BasicRepeatingNodeExample Result {counter++}: {response.Result}");
-		OutputRolls(response);
-	}
-}
-
-static void OutputRolls(NodeResponse response)
-{
-	foreach (var (_, rolls) in response.RollResponses!)
-	{
-		Console.WriteLine($"Die Size: {rolls.First().DieSize}: {string.Join(", ", rolls.Select(x => x.Result.ToString()))}");
-	}
+	Console.WriteLine(formatter.Format(responses, "BasicRepeatingNodeExample"));
 }
---
+++ src/Core/Formatter/IResponseFormatter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Dicer.Models;
+
+namespace Dicer.Formatter;
+
+public interface IResponseFormatter
+{
+	string Format(NodeResponse response, params string[] args);
+
+	string Format(IEnumerable<NodeResponse> responses, params string[] args);
+}
