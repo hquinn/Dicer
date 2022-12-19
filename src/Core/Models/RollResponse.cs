2ab64d1d95453acfe9b4dcc25f36956cbf870b6e
Hayden Quinn <hayden@onlineautoparts.com.au>
2022-12-19 14:10:00 +1000

    💡 Updated Discarded comment

--- src/Core/Models/RollResponse.cs
+++ src/Core/Models/RollResponse.cs
@@ -7,7 +7,8 @@ namespace Dicer;
 ///     Final result for a roll.
 /// </summary>
 /// <param name="Result">The result of the roll.</param>
-/// <param name="Rolls">All the rolls for the response.</param>
+/// <param name="Rolls">All the rolls for the response that was kept.</param>
+/// <param name="Discarded">All the rolls for the response that was discarded.</param>
 public record RollResponse(int Result, IEnumerable<Roll> Rolls, IEnumerable<Roll> Discarded)
 {
 	internal static RollResponse CreateResponse(IEnumerable<Roll> rolls, IEnumerable<Roll> discarded)
